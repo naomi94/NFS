f34a20904f86da1264b74ffefd1b62a1e0d82fb1
naomi94 <naomisontrop@yahoo.com>
2015-11-25 21:30:36 +0200

    Created solution

---
+++ NFS.CoreService/NfsCoreService.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceProcess;
+
+namespace NFS.CoreService
+{
+    public partial class NfsCoreService : ServiceBase
+    {
+        public NfsCoreService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
